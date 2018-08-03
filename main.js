15892094306e28b24c68e7458cc114751e8a4a60
agomez <aldogomez@outlook.com>
2018-08-03 15:38:16 -0500

    Mi primer commit

---
+++ main.js
@@ -0,0 +1,17 @@
+/*var nombre = prompt('¿Comó te llamas?');
+    var pais = prompt('¿De dónde eres?');
+    alert("Bienvenido " + nombre + " de " + pais);
+*/
+/*
+var modelo;
+modelo ='camaro';
+console.log(modelo);
+*/
+
+var auto = {
+    nombre : 'Camaro',
+    motor : 5.5,
+    color : 'Azul',
+    clasico : true,
+    anio : 1965
+}
